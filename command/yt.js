5e8ceb99db87ff757346fdbfa919c736fc2bf2c1
Shadowfacts <akorde@icloud.com>
2015-02-08 14:06:53 -0500

    !yt id
    Retrieve information about a YouTube video

---
+++ command/yt.js
@@ -0,0 +1,25 @@
+// Modules
+var app = require('../app'),
+    config = require('../config'),
+    request = require('request');
+
+exports.mode = "";
+exports.run = run;
+
+function run(channel, sender, args) {
+    var id = args[0];
+    var url = 'https://www.googleapis.com/youtube/v3/videos?id=' + id + '&key=' + config.ytKey + '&part=snippet';
+    request(url, function(error, response, body) {
+        try {
+            var json = JSON.parse(body);
+
+            var ytChannel = json.items[0].snippet.channelTitle;
+            var title = json.items[0].snippet.title;
+            var url = 'http://youtu.be/' + id;
+
+            app.bot.say(channel, 'YouTube - ' + ytChannel + ': ' + title + ' - ' + url);
+        } catch (ex) {
+            app.bot.say(channel, 'Uh oh, something wen\'t wrong.');
+        }
+    });
+}
