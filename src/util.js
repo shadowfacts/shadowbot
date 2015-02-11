83ddfb7bbc3ad92f6211234cdb2b663c56f80cef
Shadowfacts <akorde@icloud.com>
2015-02-10 21:48:47 -0500

    Stuff

--- src/util.js
+++ src/util.js
@@ -50,27 +50,31 @@ exports.autoPermissions = function(channel, who) {
 var youtubeRegexp = /^(https?\:\/\/)?((www\.)?youtube\.com)\/.+$/;
 var youtubeShortRegexp = /^(https?\:\/\/)?(youtu\.be)\/.+$/;
 exports.tryYouTube = function(from, to, text) {
-    var valid, id;
+    if (config.youtubeRegex[to]) {
+        var valid, id;
 
-    if (text.match(youtubeRegexp)) { // youtube.com
-        if (text.indexOf('v=') != -1) {
-            valid = true;
-            var temp = text.split('v=');
-            id = temp[temp.length - 1];
+        if (text.match(youtubeRegexp)) { // youtube.com
+            if (text.indexOf('v=') != -1) {
+                valid = true;
+                var temp = text.split('v=');
+                id = temp[temp.length - 1];
+            }
+        } else if (text.match(youtubeShortRegexp)) { // youtu.be
+            if (text.indexOf('/') != -1) {
+                valid = true;
+                var temp = text.split('/');
+                id = temp[temp.length - 1];
+            }
         }
-    } else if (text.match(youtubeShortRegexp)) { // youtu.be
-        if (text.indexOf('/') != -1) {
-            valid = true;
-            var temp = text.split('/');
-            id = temp[temp.length - 1];
+
+        if (valid) {
+            commandHandler.handle(to, from, 'yt', [id]);
         }
-    }
 
-    if (valid) {
-        commandHandler.handle(to, from, 'yt', [id]);
+        return valid;
+    } else {
+        return false;
     }
-
-    return valid;
 }
 
 // GitHub
