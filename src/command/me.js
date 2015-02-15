117bc54d3ef1ddce9af40051c3fbd3a783b867c9
Shadowfacts <akorde@icloud.com>
2015-02-14 19:33:15 -0500

    v2 Reset

--- src/command/me.js
+++
@@ -1,9 +0,0 @@
-// Modules
-var app = require('../app');
-
-exports.mode = "+";
-exports.run = run;
-
-function run(channel, sender, args) {
-    app.bot.action(channel, args.join(' '))
-}
--- src/command/shadowbot.js
+++
@@ -1,9 +0,0 @@
-// Modules
-var app = require('../app');
-
-exports.mode = "";
-exports.run = run;
-
-function run(channel, sender, args) {
-    app.bot.say(channel, "Hello, I'm shadowbot, I'm a robot. Look at my source code here: http://git.io/b50u!");
-}
